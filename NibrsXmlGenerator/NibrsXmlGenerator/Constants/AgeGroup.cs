3741838a9bdf6280b65bfc5b16e5f8d1f8a56a38
Ivan Filpo <ifilpo@teisd.com>
2016-08-24 16:16:24 -0500

    Defined age group keys or headers for UCR Online and report consumption.

---
+++ NibrsXmlGenerator/NibrsXmlGenerator/Constants/AgeGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NibrsXml.Constants
+{
+    public class AgeGroup
+    {
+        public const string UnderTen = "Under 10";
+        public const string TenToTwelve = "10-12";
+        public const string ThirteenToFourteen = "13-14";
+        public const string Fithteen = "15";
+        public const string Sixteen = "16";
+        public const string Seventeen = "17";
+        public const string Eighteen = "18";
+        public const string Nineteen = "19";
+        public const string Twenty = "20";
+        public const string Twentyone = "21";
+        public const string Twentytwo = "22";
+        public const string Twentythree= "23";
+        public const string Twentyfour = "24";
+        public const string TwentyfiveToTwentynine = "25-29";
+        public const string ThirtyToThirtyfour = "30-34";
+        public const string ThirtyfiveToThirtynine = "35-39";
+        public const string FortyToFortyfour = "40-44";
+        public const string FortyfiveToFortynine = "45-49";
+        public const string FiftyToFiftyfour = "50-54";
+        public const string FiftyfiveToFiftynine = "55-59";
+        public const string SixtyToSixtyfour = "60-64";
+        public const string SixtyfivePlus = "65+";
+    }
+}
--- NibrsXmlGenerator/NibrsXmlGenerator/Ucr/DataCollections/Asre.cs
+++ NibrsXmlGenerator/NibrsXmlGenerator/Ucr/DataCollections/Asre.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NibrsXml.Utility;
+using NibrsXml.Constants;
 
 namespace NibrsXml.Ucr.DataCollections
 {
@@ -24,8 +25,140 @@ namespace NibrsXml.Ucr.DataCollections
 
         public void AddCounts(string age, string sex, string race, string ethnicity)
         {
-            // todo: parse counts into ranges for ages 25+ before adding it to ageSexCounts
-            ageSexCounts.TryAdd(age).TryIncrement(sex);
+            var ageGroup = age.TrimStart('0');
+            switch (ageGroup)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    ageGroup =  AgeGroup.UnderTen;
+                    break;
+                case "10":
+                case "11":
+                case "12":
+                    ageGroup =  AgeGroup.TenToTwelve;
+                    break;
+                case "13":
+                case "14":
+                    ageGroup =  AgeGroup.ThirteenToFourteen;
+                    break;
+                case "15":
+                case "16":
+                case "17":
+                case "18":
+                case "19":
+                case "20":
+                case "21":
+                case "22":
+                case "23":
+                case "24":
+                    ageGroup = age;
+                    break;
+                case "25":
+                case "26":
+                case "27":
+                case "28":
+                case "29":
+                    ageGroup = AgeGroup.TwentyfiveToTwentynine;
+                    break;
+                case "30":
+                case "31":
+                case "32":
+                case "33":
+                case "34":
+                    ageGroup = AgeGroup.ThirtyToThirtyfour;
+                    break;
+                case "35":
+                case "36":
+                case "37":
+                case "38":
+                case "39":
+                    ageGroup = AgeGroup.ThirtyfiveToThirtynine;
+                    break;
+                case "40":
+                case "41":
+                case "42":
+                case "43":
+                case "44":
+                    ageGroup = AgeGroup.FortyToFortyfour;
+                    break;
+                case "45":
+                case "46":
+                case "47":
+                case "48":
+                case "49":
+                    ageGroup = AgeGroup.FortyfiveToFortynine;
+                    break;
+                case "50":
+                case "51":
+                case "52":
+                case "53":
+                case "54":
+                    ageGroup = AgeGroup.FiftyToFiftyfour;
+                    break;
+                case "55":
+                case "56":
+                case "57":
+                case "58":
+                case "59":
+                    ageGroup = AgeGroup.FiftyfiveToFiftynine;
+                    break;
+                case "60":
+                case "61":
+                case "62":
+                case "63":
+                case "64":
+                    ageGroup = AgeGroup.SixtyToSixtyfour;
+                    break;
+                case "65":
+                case "66":
+                case "67":
+                case "68":
+                case "69":
+                case "70":
+                case "71":
+                case "72":
+                case "73":
+                case "74":
+                case "75":
+                case "76":
+                case "77":
+                case "78":
+                case "79":
+                case "80":
+                case "81":
+                case "82":
+                case "83":
+                case "84":
+                case "85":
+                case "86":
+                case "87":
+                case "88":
+                case "89":
+                case "90":
+                case "91":
+                case "92":
+                case "93":
+                case "94":
+                case "95":
+                case "96":
+                case "97":
+                case "98":
+                case "99":
+                    ageGroup = AgeGroup.SixtyfivePlus;
+                    break;
+                default:
+                    return;
+
+            }
+
+            ageSexCounts.TryAdd(ageGroup).TryIncrement(sex);
             raceCounts.TryIncrement(race);
             ethnicityCounts.TryIncrement(ethnicity);
             TotalCount += 1;
