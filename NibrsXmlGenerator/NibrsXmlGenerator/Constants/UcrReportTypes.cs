5a04a6aff9aef5e20cca668a34176e2da3c3544b
Ivan Filpo <ifilpo@teisd.com>
2017-03-16 17:11:59 -0500

    1. Further enhanced the UcrReportType enum in NibrsXml constants to use with UCR Generator program. 2. Enhanced NibrsImport.cs to always use the ReadDirectoryPath to find the schema (xsd) directory. This should change to a website reference in the future and become more of a global static function to be used all over the projects where NibrsXml is used.

--- NibrsXmlGenerator/NibrsXmlGenerator/Constants/UcrReportTypes.cs
+++ NibrsXmlGenerator/NibrsXmlGenerator/Constants/UcrReportTypes.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LoadBusinessLayer;
+using NibrsXml.Utility;
 
 namespace NibrsXml.Constants
 {
     public enum UcrReportType
     {
-        Arson,
-        Asra,
-        Asrj,
-        HateCrime,
-        Homicide,
-        HumanTrafficking,
-        Leoka,
+        [UcrReport("NibrsXml.Ucr.Reports.ReturnA.returna.xsl","_ReturnA.xml","_ReturnA.html")]
         ReturnA,
-        ReturnASupplement
+
+        [UcrReport("NibrsXml.Ucr.Reports.ReturnA.returnasupp.xsl","_ReturnASupplement.xml","_ReturnASupplement.html")]
+        ReturnASupplement,
+
+        [UcrReport("NibrsXml.Ucr.Reports.Asre.asre.xsl","_Asre.xml","_Asre.html")]
+        Asre,
+
+        [UcrReport("NibrsXml.Ucr.Reports.Arson.arson.xsl","_Arson.xml","_Arson.html")]
+        Arson,
+
+        [UcrReport("NibrsXml.Ucr.Reports.HumanTrafficking.ht.xsl","_HumanTrafficking.xml","_HumanTrafficking.html")]
+        HumanTrafficking,
+
+        HateCrime,
+
+        SupplementaryHomicide,
+
+        [UcrReport("NibrsXml.Ucr.Reports.Leoka.leoka.xsl","_Leoka.xml","_Leoka.html")]
+        Leoka
     }
 }
--- NibrsXmlGenerator/NibrsXmlGenerator/Ucr/NibrsImport.cs
+++ NibrsXmlGenerator/NibrsXmlGenerator/Ucr/NibrsImport.cs
@@ -10,6 +10,7 @@ using NibrsXml.NibrsReport;
 using NibrsXml.Ucr.DataCollections;
 using NibrsXml.Ucr.DataMining;
 using NibrsXml.Utility;
+using System.Configuration;
 
 namespace NibrsXml.Ucr
 {
@@ -30,8 +31,24 @@ namespace NibrsXml.Ucr
 
         public NibrsImport(Submission submission)
         {
+            var schemasToUseForValidation = new List<KeyValuePair<String, String>>()
+            {
+                new KeyValuePair<String, String>("http://fbi.gov/cjis/nibrs/4.0", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\nibrs\4.0\nibrs.xsd"),
+                new KeyValuePair<String, String>("http://fbi.gov/cjis/1.0", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\cjis\1.0\cjis.xsd"),
+                new KeyValuePair<String, String>("http://fbi.gov/cjis/cjis-codes/1.0", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\cjis\1.0\cjis-codes.xsd"),
+                new KeyValuePair<String, String>("http://release.niem.gov/niem/appinfo/3.0/", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\niem\appinfo\3.0\appinfo.xsd"),
+                new KeyValuePair<String, String>("http://release.niem.gov/niem/codes/fbi_ucr/3.0/", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\niem\codes\fbi_ucr\3.0\fbi_ucr.xsd"),
+                new KeyValuePair<String, String>("http://release.niem.gov/niem/domains/jxdm/5.1/", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\niem\domains\jxdm\5.1\jxdm.xsd"),
+                new KeyValuePair<String, String>("http://release.niem.gov/niem/localTerminology/3.0/",
+                    ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\niem\localTerminology\3.0\localTerminology.xsd"),
+                new KeyValuePair<String, String>("http://release.niem.gov/niem/niem-core/3.0/", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\niem\niem-core\3.0\niem-core.xsd"),
+                new KeyValuePair<String, String>("http://release.niem.gov/niem/proxy/xsd/3.0/", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\niem\proxy\xsd\3.0\xs.xsd"),
+                new KeyValuePair<String, String>("http://release.niem.gov/niem/structures/3.0/", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\niem\structures\3.0\structures.xsd"),
+                new KeyValuePair<String, String>("http://fbi.gov/cjis/nibrs/nibrs-codes/4.0", ConfigurationManager.AppSettings[@"ReadDirectoryPath"] + @"xsd\nibrs\4.0\nibrs-codes.xsd"),
+            };
+
             //Use the StringReader Overload constructor to validate the string directly instead of reading an xml file.
-            XmlValidator validator = new XmlValidator(new StringReader(submission.Xml));
+            XmlValidator validator = new XmlValidator(new StringReader(submission.Xml), schemasToUseForValidation);
             if (!validator.HasErrors)
             {
                 Reports = submission.Reports.Where(r => r.Header.ReportActionCategoryCode == ReportActionCategoryCode.I.NibrsCode()).ToList();
