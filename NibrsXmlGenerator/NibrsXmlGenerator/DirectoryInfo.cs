9d671e2f6b7e2c5a96d7802819f95416cf195f6a
Sharuk Shaik <sshaik@teisd.com>
2020-06-11 12:30:13 -0500

    Added Class to get Agency Directory Info

---
+++ NibrsXmlGenerator/NibrsXmlGenerator/DirectoryInfo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NibrsXml
+{
+  public  class AgencyXmlDirectoryInfo
+    {
+
+        public readonly AppSettingsReader AppSettingsReader = new AppSettingsReader();
+
+        public string NibrsXmlFolderPath { get; private set; }
+
+        public AgencyXmlDirectoryInfo(string ori)
+        {
+            var appSettingsReader = new AppSettingsReader();
+            var nibrsXmlFilesFolderLocation = Convert.ToString(appSettingsReader.GetValue("IncomingNibrsXmlFilesFolderLocation", typeof(string)));
+            var strIncomingFolderLocation = Convert.ToString(appSettingsReader.GetValue("ReadDirectoryPath", typeof(string)));
+         
+
+            NibrsXmlFolderPath = strIncomingFolderLocation + ori + "\\" + nibrsXmlFilesFolderLocation;
+        }
+
+
+        public string GetFailedToUploadLocation()
+        {
+
+            var failedToUploadLocation = Convert.ToString(AppSettingsReader.GetValue("FailedToUploadNibrsXmlFilesFolderLocation", typeof(string)));
+            return NibrsXmlFolderPath + "\\" + failedToUploadLocation;
+        }
+
+
+        public DirectoryInfo GetFailedToUploadDirectory()        
+        {
+
+            var failedToUploadLocation = Convert.ToString(AppSettingsReader.GetValue("FailedToUploadNibrsXmlFilesFolderLocation", typeof(string)));
+            DirectoryInfo directory = new DirectoryInfo(NibrsXmlFolderPath + "\\" + failedToUploadLocation);
+            if (!directory.Exists)
+                directory.Create();
+            return directory;
+        }
+
+
+        public string GetFailedToSaveLocation()
+        {
+            var failedToSaveLocation = Convert.ToString(AppSettingsReader.GetValue("FailedToSaveNibrsXmlFilesFolderLocation", typeof(string)));
+            return NibrsXmlFolderPath + "\\" + failedToSaveLocation;
+        }
+
+
+
+        public DirectoryInfo GetFailedToSaveDirectory()
+        {
+
+            var failedToSaveLocation = Convert.ToString(AppSettingsReader.GetValue("FailedToSaveNibrsXmlFilesFolderLocation", typeof(string)));
+            DirectoryInfo directory = new DirectoryInfo(NibrsXmlFolderPath + "\\" + failedToSaveLocation);
+            if (!directory.Exists)
+                directory.Create();
+            return directory;
+        }
+
+
+        public string GetArchiveLocation()
+        {
+            var archiveXmlLocation = Convert.ToString(AppSettingsReader.GetValue("ArchiveNibrsXmlFilesFolderLocation", typeof(string)));
+            return NibrsXmlFolderPath + "\\" + archiveXmlLocation;
+        }
+    }
+}
---
+++ NibrsXmlGenerator/NibrsXmlGenerator/Utility/NibrsGenericExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace NibrsXml.Utility
+{
+    public static class NibrsGenericExtensions
+    {
+        #region String Extensions
+
+        public static string TrimNullIfEmpty(this string input)
+        {
+            return input.Trim() == string.Empty ? null : input.Trim();
+        }
+
+        // String Extensions
+        public static T TryBuild<T>(this string input)
+        {
+            if (input.Trim() != string.Empty)
+                return (T) Activator.CreateInstance(typeof(T), input);
+            return default(T);
+        }
+
+        public static T TryBuild<T>(this string input, params string[] args)
+        {
+            if (input.Trim() != string.Empty)
+                return (T) Activator.CreateInstance(typeof(T), input, args);
+            return default(T);
+        }
+
+        #endregion
+
+        #region Enum Extensions
+
+        public static string NibrsCode(this Enum e)
+        {
+            return NibrsCodeAttribute.GetDescription(e);
+        }
+
+        public static string NibrsCodeDescription(this Enum e)
+        {
+            return CodeDescriptionAttribute.GetDescription(e);
+        }
+
+        public static string NibrsTextDescription(this Enum e)
+        {
+            return TextDescriptionAttribute.GetDescription(e);
+        }
+
+        public static string UcrReportHeader(this Enum e)
+        {
+            return UcrElementName.GetDescription(e);
+        }
+
+        #endregion
+    }
+}
