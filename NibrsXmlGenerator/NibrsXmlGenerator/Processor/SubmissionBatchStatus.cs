de6cf75bc109421abe1ea6fda7f028c433352f67
Sharuk Shaik <sshaik@teisd.com>
2020-08-24 11:11:49 -0500

    Added Submission Batch Status and DeleteTransformer.cs

---
+++ NibrsXmlGenerator/NibrsXmlGenerator/Processor/SubmissionBatchStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace NibrsXml.Processor
+{
+    public class SubmissionBatchStatus
+    {
+        public string RunNUmber { get; set; }
+
+        public int NoOfSubmissions { get; set; }
+
+        public string Ori { get; set; }
+
+        public string Environmennt { get; set; }
+
+        public bool HasErrorOccured { get; set; }
+
+    }
+}
