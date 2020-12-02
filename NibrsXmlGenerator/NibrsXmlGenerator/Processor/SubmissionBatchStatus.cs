4bab5364b4442c0418b5cb72a73969e526c072a3
Sharuk Shaik <sshaik@teisd.com>
2020-12-02 14:55:14 -0600

    Removed unnecessary properties of the SubmissionBatchStatus

--- NibrsXmlGenerator/NibrsXmlGenerator/Processor/SubmissionBatchStatus.cs
+++ NibrsXmlGenerator/NibrsXmlGenerator/Processor/SubmissionBatchStatus.cs
@@ -2,14 +2,7 @@
 {
     public class SubmissionBatchStatus
     {
-        public string RunNumber { get; set; }
-
-        public int NoOfSubmissions { get; set; }
-
         public string Ori { get; set; }
-
-        public string Environmennt { get; set; }
-
         public bool HasErrorOccured { get; set; }
 
     }
