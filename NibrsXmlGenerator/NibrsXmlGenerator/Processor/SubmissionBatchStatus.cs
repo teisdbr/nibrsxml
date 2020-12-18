31e487e00681491c64e2b896b47355b7dde71f41
Sharuk Shaik <sshaik@teisd.com>
2020-12-17 19:47:16 -0600

    Reverted back the SubmissionBatchStatus properties

--- NibrsXmlGenerator/NibrsXmlGenerator/Processor/SubmissionBatchStatus.cs
+++ NibrsXmlGenerator/NibrsXmlGenerator/Processor/SubmissionBatchStatus.cs
@@ -2,7 +2,14 @@
 {
     public class SubmissionBatchStatus
     {
+        public string RunNumber { get; set; }
+
+        public int NoOfSubmissions { get; set; }
+
         public string Ori { get; set; }
+
+        public string Environmennt { get; set; }
+
         public bool HasErrorOccured { get; set; }
 
     }
